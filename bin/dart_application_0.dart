4b98a994c99036e25c5e0f8374edd50c1660f47b
FlorangelValverde <florangelvalverde@upeu.edu.pe>
2021-10-21 20:58:19 -0500

    dart

---
+++ bin/dart_application_0.dart
@@ -0,0 +1,13 @@
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
+void main(List<String> arguments) {
+  final url = Uri.parse('https://reqres.in/api/users?page=2');
+  http.get(url).then((res) {
+    final body = convert.jsonDecode(res.body);
+    for (int i=0; i<5; i++){
+    print('${body ['data'] [i]['email']}');
+    }
+   
+  });
+}
